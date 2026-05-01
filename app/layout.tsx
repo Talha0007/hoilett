60da631ae7a6bd0da5a8e7722179c5c7cb14ac20
Talha0007 <talha.hameed068@gmail.com>
2026-05-01 20:07:44 +0500

    disabled visitor tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
       <body className="bg-black">
-        <VisitorTracker />
+        {/* <VisitorTracker /> */}
         <Navbar />
         {children}
         <ChatWidget />
